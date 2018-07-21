210fe1e312f5cf9ba3fcce99c2c8c16ac7de6edd
大須賀善揮 <osugayoshiki@yoshikiosuga.local>
2018-07-21 12:13:43 +0900

    mail件名内容変更

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -2,6 +2,6 @@ class PictureMailer < ApplicationMailer
   def picture_mail(picture, email)
    @picture = picture
 
-   mail to: email, subject: "お問い合わせの確認メール"
+   mail to: email, subject: "画像投稿の確認メール"
   end
 end
